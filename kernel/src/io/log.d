3010fdf9495cba3eb8dd54c5b5947e6c3034695d
Dan Printzell <xwildn00bx@gmail.com>
2015-11-22 20:09:59 +0100

    [io/log] Updated log write function to use variadic args
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- kernel/src/io/log.d
+++ kernel/src/io/log.d
@@ -2,6 +2,7 @@ module io.log;
 
 import io.com;
 import data.string;
+import data.util;
 
 __gshared Log log;
 
@@ -22,7 +23,7 @@ struct Log {
 		indent = 0;
 	}
 
-	void opCall(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(LogLevel level, string msg = "") {
+	void opCall(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(LogLevel level, Arg args) {
 		for (int i = 0; i < indent; i++)
 			COM1.Write(' ');
 
@@ -33,33 +34,53 @@ struct Log {
 		for (size_t i = start; i < buf.length; i++)
 			COM1.Write(buf[i]);
 
-		COM1.Write("> ", msg);
+		COM1.Write("> ");
+		foreach (arg; args) {
+			alias T = Unqual!(typeof(arg));
+			static if (is(T : const char[]))
+				COM1.Write(arg);
+			/*else static if (is(T == enum))
+				WriteEnum(arg);*/
+			else static if (is(T : V *, V)) {
+				COM1.Write("0x");
+				start = itoa(cast(ulong)arg, buf.ptr, buf.length, 16);
+				for (size_t i = start; i < buf.length; i++)
+					COM1.Write(buf[i]);
+			} else static if (is(T : char))
+				COM1.Write(arg);
+			else static if (isNumber!T) {
+				start = itoa(arg, buf.ptr, buf.length, 10);
+				for (size_t i = start; i < buf.length; i++)
+					COM1.Write(buf[i]);
+			} else
+				COM1.Write("UNKNOWN TYPE '", T.stringof, "'");
+		}
 
 		COM1.Write("\r\n");
 	}
 
-	void Verbose(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.VERBOSE, msg);
+	void Verbose(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.VERBOSE, args);
 	}
 
-	void Debug(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.DEBUG, msg);
+	void Debug(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.DEBUG, args);
 	}
 
-	void Info(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.INFO, msg);
+	void Info(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.INFO, args);
 	}
 
-	void Warning(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.WARNING, msg);
+	void Warning(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.WARNING, args);
 	}
 
-	void Error(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.ERROR, msg);
+	void Error(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.ERROR, args);
 	}
 
-	void Fatal(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__)(string msg = "") {
-		this.opCall!(file, func, line)(LogLevel.FATAL, msg);
+	void Fatal(string file = __FILE__, string func = __PRETTY_FUNCTION__, int line = __LINE__, Arg...)(Arg args) {
+		this.opCall!(file, func, line)(LogLevel.FATAL, args);
 	}
 
 }
