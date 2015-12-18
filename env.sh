0c30c89ba035274d26f356331bfdc589608d1d52
Dan Printzell <xwildn00bx@gmail.com>
2015-12-18 23:41:10 +0100

    [env.sh] Build shortcut without running
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
+function v() {rm -rf powernex.krl kernel/obj/ .wild-cache && ./build}
 function b() {rm -rf powernex.krl kernel/obj/ .wild-cache && ./build && qemu-system-x86_64 -cdrom powernex.iso -m 512 -monitor stdio}
 function n() {rm -rf powernex.krl kernel/obj/ .wild-cache bochsout.txt && ./build && bochs -f bochsrc.txt}
