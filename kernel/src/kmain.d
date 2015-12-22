3de3a3fc792aea36a7d7a77a5991c157f2ed71c4
Dan Printzell <xwildn00bx@gmail.com>
2015-12-23 00:58:46 +0100

    [kmain] Added testing of paging
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- kernel/src/kmain.d
+++ kernel/src/kmain.d
@@ -5,6 +5,10 @@ import io.textmode;
 import cpu.gdt;
 import cpu.idt;
 import multiboot;
+import memory.paging;
+import memory.frameallocator;
+import linker;
+import data.address;
 
 alias scr = GetScreen;
 alias gdt = GDT;
@@ -17,14 +21,36 @@ extern (C) int kmain(uint magic, ulong info) {
 	scr.Clear();
 	gdt.Init();
 	idt.Init();
-
+	Multiboot.ParseHeader(magic, info);
+	FrameAllocator.Init(Multiboot.memorySize);
 	scr.Writeln("Welcome to PowerNex!");
 	scr.Writeln("\tThe number one D kernel!");
-	scr.Write("Compiled using '", __VENDOR__, "', D version ", major, ".", minor, "\n");
+	scr.Writeln("Compiled using '", __VENDOR__, "', D version ", major, ".", minor, "\n");
+
+	scr.Writeln("DefaultKernel: ", cast(void*)MapMode.DefaultKernel);
+	scr.Writeln("DefaultUser: ", cast(void*)MapMode.DefaultUser);
 
 	log.Info("Welcome to PowerNex's serial console!");
 
-	Multiboot.ParseHeader(magic, info);
+	Paging kernelPaging = GetKernelPaging();
+
+	kernelPaging.Install();
+
+	scr.Writeln();
+	scr.Writeln();
+
+	scr.color.Foreground = Colors.Green;
+	scr.Writeln("\t\t\t\tWorks!");
+
+	kernelPaging.Map(VirtAddress(0xA_0000_0000), PhysAddress(0x0), MapMode.DefaultUser);
+	kernelPaging.Unmap(VirtAddress(0xA_0000_0000));
+	scr.Writeln("\t\t\t\tWorks!");
+
+	kernelPaging.MapFreeMemory(VirtAddress(0xB_0000_0000), MapMode.DefaultUser);
+	int* test = cast(int*)0xB_0000_0000;
+	*test = 0xDEAD_BEEF;
+	kernelPaging.Unmap(VirtAddress(0xB_0000_0000));
+	scr.Writeln("\t\t\t\tWorks!");
 
 	asm {
 	forever:
