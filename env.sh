df0c9f2d909809c8fe5bf0c1ab847d3c1fda76b4
Dan Printzell <xwildn00bx@gmail.com>
2016-02-14 00:07:36 +0100

    [ENV] Added logging of COM1
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 function v() {rm -rf powernex.krl kernel/obj/ .wild-cache && ./build}
-function b() {rm -rf powernex.krl kernel/obj/ .wild-cache && ./build && qemu-system-x86_64 -cdrom powernex.iso -m 512 -monitor stdio}
+function b() {rm -rf powernex.krl kernel/obj/ .wild-cache && ./build && qemu-system-x86_64 -cdrom powernex.iso -m 2048 -monitor stdio -serial file:com1.log -no-reboot}
 function n() {rm -rf powernex.krl kernel/obj/ .wild-cache bochsout.txt && ./build && bochs -f bochsrc.txt}
