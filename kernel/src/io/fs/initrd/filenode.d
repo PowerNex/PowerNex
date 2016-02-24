108a94cb61d1fd3293e944d14dd002f55867c38e
Dan Printzell <xwildn00bx@gmail.com>
2016-02-24 21:43:24 +0100

    [FS/Initrd] Added initrd image reading
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

---
+++ kernel/src/io/fs/initrd/filenode.d
@@ -0,0 +1,41 @@
+module io.fs.initrd.filenode;
+
+import io.fs.initrd;
+import io.fs;
+
+final class InitrdFileNode : FileNode {
+public:
+	this(InitrdRootNode root, ulong id, string name, ubyte* offset, ulong size, DirectoryNode parent) {
+		this.root = root;
+		this.data = offset[0 .. size];
+		super(id, name, NodePermissions(PermissionMask(Mask.RWX, Mask.RX, Mask.RX), 0UL, 0UL), size, parent);
+		this.size = size;
+	}
+
+	override ulong Read(ubyte[] buffer, ulong offset) {
+		ulong size = buffer.length;
+		ulong end = size + offset;
+		if (end > data.length) {
+			end = data.length;
+			size = end - offset;
+		}
+
+		memcpy(&buffer[offset], data.ptr, size);
+
+		return size;
+	}
+
+	override ulong Write(ubyte[] buffer, ulong offset) {
+		return -1;
+	}
+
+	override void Open() {
+	}
+
+	override void Close() {
+	}
+
+private:
+	InitrdRootNode root;
+	ubyte[] data;
+}
---
+++ kernel/src/io/fs/initrd/package.d
@@ -0,0 +1,7 @@
+module io.fs.initrd;
+
+public {
+	import io.fs.initrd.rootnode;
+	import io.fs.initrd.filenode;
+	import io.fs.initrd.directorynode;
+}
