b2214c72e2511664e4e763001fc11a7debeaed49
Dan Printzell <xwildn00bx@gmail.com>
2016-03-01 23:53:31 +0100

    [KMain] Initializing PIT and Keyboard and added test for them
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- Kernel/src/KMain.d
+++ Kernel/src/KMain.d
@@ -2,8 +2,10 @@ module KMain;
 
 import IO.Log;
 import IO.TextMode;
+import IO.Keyboard;
 import CPU.GDT;
 import CPU.IDT;
+import CPU.PIT;
 import Data.Multiboot;
 import Memory.Paging;
 import Memory.FrameAllocator;
@@ -20,6 +22,30 @@ extern (C) int kmain(uint magic, ulong info) {
 	PreInit();
 	Welcome();
 	Init(magic, info);
+	asm {
+		sti;
+	}
+
+	scr.Writeln();
+	scr.Writeln("User input:");
+
+	char key;
+	VirtAddress timePosition = VirtAddress(0xFFFF_FFFF_800B_8000) + (80 * 1 - 1) * 2;
+	while (key != 27 /* Escape */ ) {
+		// Print out seconds since boot, in the top right corner
+		ulong tmp = PIT.Seconds;
+		*((timePosition - (0 * 2)).Ptr!ubyte) = '0' + tmp % 10;
+		tmp /= 10;
+		*((timePosition - (1 * 2)).Ptr!ubyte) = '0' + tmp % 10;
+		tmp /= 10;
+		*((timePosition - (2 * 2)).Ptr!ubyte) = '0' + tmp % 10;
+
+		// Get User input and write it out
+		key = Keyboard.Get();
+		if (key)
+			scr.Write(key);
+	}
+	scr.Writeln();
 
 	scr.color.Foreground = Colors.Magenta;
 	scr.color.Background = Colors.Yellow;
@@ -29,9 +55,16 @@ extern (C) int kmain(uint magic, ulong info) {
 
 void PreInit() {
 	scr.Clear();
-	GDT.Init();
-	IDT.Init();
+	scr.Writeln("Log initializing...");
 	log.Init();
+	scr.Writeln("GDT initializing...");
+	GDT.Init();
+	scr.Writeln("IDT initializing...");
+	IDT.Init();
+	scr.Writeln("PIT initializing...");
+	PIT.Init();
+	scr.Writeln("Keyboard initializing...");
+	Keyboard.Init();
 }
 
 void Welcome() {
@@ -44,13 +77,25 @@ void Welcome() {
 }
 
 void Init(uint magic, ulong info) {
+	scr.Writeln("Multiboot parsing...");
 	Multiboot.ParseHeader(magic, info);
+
+	scr.Writeln("FrameAllocator initializing...");
 	FrameAllocator.Init();
+
 	auto symbols = Multiboot.GetModule("symbols");
-	if (symbols[0] != symbols[1])
+	if (symbols[0] != symbols[1]) {
+		scr.Writeln("Found debug symbols for log!");
 		log.SetSymbolMap(symbols[0], symbols[1]);
+	}
+
+	scr.Writeln("Paging initializing...");
 	GetKernelPaging.Install();
+
+	scr.Writeln("Heap initializing...");
 	GetKernelHeap;
+
+	scr.Writeln("Initrd initializing...");
 	LoadInitrd();
 }
 
