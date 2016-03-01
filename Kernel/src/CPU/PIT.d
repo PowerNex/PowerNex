9c2e941a8fafcefda182036a4186be256def420c
Dan Printzell <xwildn00bx@gmail.com>
2016-03-01 23:52:50 +0100

    [PIT] Added PIT handler class
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

---
+++ Kernel/src/CPU/PIT.d
@@ -0,0 +1,37 @@
+module CPU.PIT;
+
+import CPU.IDT;
+import IO.Port;
+import IO.Log;
+import Data.Register;
+
+struct PIT {
+public:
+	static void Init(uint hz = 100) {
+		uint IRQ0 = 32;
+		IDT.Register(IRQ0, &onTick);
+		this.hz = hz;
+		uint divisor = 1193180 / hz;
+		Out!ubyte(0x43, 0x36);
+
+		ubyte l = cast(ubyte)(divisor & 0xFF);
+		ubyte h = cast(ubyte)((divisor >> 8) & 0xFF);
+
+		Out!ubyte(0x40, l);
+		Out!ubyte(0x40, h);
+	}
+
+	static @property ulong Seconds() {
+		if (hz)
+			return counter / hz;
+		return 0;
+	}
+
+private:
+	__gshared bool enabled;
+	__gshared uint hz;
+	__gshared ulong counter;
+	static void onTick(InterruptRegisters* regs) {
+		counter++;
+	}
+}
