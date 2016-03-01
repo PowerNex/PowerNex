fcfad61e164938640555e5c999766c5e302cfbfa
Dan Printzell <xwildn00bx@gmail.com>
2016-03-01 23:49:37 +0100

    [Register] Added support for getting CR2
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- Kernel/src/Data/Register.d
+++ Kernel/src/Data/Register.d
@@ -1,9 +1,15 @@
 module Data.Register;
 
+private extern (C) void* CPU_ret_cr2();
+
 struct InterruptRegisters {
 align(1):
 	ulong R15, R14, R13, R12, R11, R10, R9, R8;
 	ulong RBP, RDI, RSI, RDX, RCX, RBX, RAX;
 	ulong IntNumber, ErrorCode;
 	ulong RIP, CS, Flags, RSP, SS;
+
+	@property void* CR2() const {
+		return CPU_ret_cr2();
+	}
 }
