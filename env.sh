f56a1d2dc9e5ac1dcd37dca592643ea906e3a315
Dan Printzell <xwildn00bx@gmail.com>
2016-03-14 22:47:12 +0100

    [Env] Updated command to match the current file structure

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-function v() {rm -rf PowerNex.krl Kernel/obj/ .wild-cache && ./build}
-function b() {rm -rf PowerNex.krl Kernel/obj/ .wild-cache && ./build && qemu-system-x86_64 -cdrom PowerNex.iso -m 2048 -monitor stdio -serial file:COM1.log -no-reboot}
-function n() {rm -rf PowerNex.krl Kernel/obj/ .wild-cache bochsout.txt && ./build && bochs -f bochsrc.txt}
-function a() {addr2line -e PowerNex.krl $1}
+function v() {rm -rf Kernel/obj/ .wild-cache && ./build}
+function b() {rm -rf Kernel/obj/ .wild-cache && ./build && qemu-system-x86_64 -cdrom PowerNex.iso -m 2048 -monitor stdio -serial file:COM1.log -no-reboot}
+function n() {rm -rf Kernel/obj/ .wild-cache bochsout.txt && ./build && bochs -f bochsrc.txt}
+function a() {addr2line -e Disk/boot/PowerNex.krl $1}
