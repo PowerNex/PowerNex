c9db4299a6e2ed0747bc66d76554c6f17da89154
Dan Printzell <xwildn00bx@gmail.com>
2016-05-20 23:27:15 +0200

    [Address] Extracted pointer type and added PhysAddress32

--- Kernel/src/Data/Address.d
+++ Kernel/src/Data/Address.d
@@ -1,23 +1,23 @@
 module Data.Address;
 
-private mixin template AddressBase() {
-	ulong addr;
+private mixin template AddressBase(Type = ulong) {
+	Type addr;
 
 	alias addr this;
 
 	this(void* addr) {
-		this.addr = cast(ulong)addr;
+		this.addr = cast(Type)addr;
 	}
 
-	this(ulong addr) {
+	this(Type addr) {
 		this.addr = addr;
 	}
 
 	typeof(this) opBinary(string op)(void* other) const {
-		return typeof(this)(mixin("addr" ~ op ~ "cast(ulong)other"));
+		return typeof(this)(mixin("addr" ~ op ~ "cast(Type)other"));
 	}
 
-	typeof(this) opBinary(string op)(ulong other) const {
+	typeof(this) opBinary(string op)(Type other) const {
 		return typeof(this)(mixin("addr" ~ op ~ "other"));
 	}
 
@@ -34,7 +34,7 @@ private mixin template AddressBase() {
 			return 0;
 	}
 
-	int opCmp(ulong other) const {
+	int opCmp(Type other) const {
 		if (Int < other)
 			return -1;
 		else if (Int > other)
@@ -48,15 +48,15 @@ private mixin template AddressBase() {
 	}
 
 	@property T* Ptr(T = void)(T addr) {
-		this.addr = cast(ulong)addr;
+		this.addr = cast(Type)addr;
 		return cast(T*)addr;
 	}
 
-	@property ulong Int() const {
+	@property Type Int() const {
 		return addr;
 	}
 
-	@property ulong Int(ulong addr) {
+	@property Type Int(Type addr) {
 		this.addr = addr;
 		return addr;
 	}
@@ -74,5 +74,14 @@ struct PhysAddress {
 	}
 }
 
+struct PhysAddress32 {
+	mixin AddressBase!uint;
+
+	@property VirtAddress Virtual() const {
+		return VirtAddress(Int + 0xFFFF_8000_0000_0000);
+	}
+}
+
 static assert(VirtAddress.sizeof == size_t.sizeof);
 static assert(PhysAddress.sizeof == size_t.sizeof);
+static assert(PhysAddress32.sizeof == uint.sizeof);
