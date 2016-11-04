6fd146dc153e61646d37d982a0e0bdd2c421ff01
Dan Printzell <xwildn00bx@gmail.com>
2016-11-04 02:48:22 +0100

    new buildsystem, ~Kinda~ works
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- env.sh
+++ env.sh
@@ -1,6 +1,10 @@
-function c() {
-		rm -rf .wild-cache PowerNex.iso Disk/boot/PowerNex.{krl,dsk} Initrd/Binary Userspace/lib {Kernel,Userspace/{Init,libRT,libPowerNex,Shell,Cat,HelloWorld}}/obj/
+function initPowerNex() {
+	if [ ! -f build.ninja ]; then
+		dub fetch reggae
+		dub run reggae -- -b ninja .
+	fi
 }
-function v() {c && ./build}
-function b() {v && qemu-system-x86_64 -cdrom PowerNex.iso -m 2048 -monitor stdio -serial file:COM1.log -no-reboot}
+function c() {initPowerNex && ninja -t clean}
+function v() {initPowerNex && ninja}
+function b() {v && qemu-system-x86_64 -cdrom PowerNex.iso -m 2048 -monitor stdio -serial file:COM1.log -no-reboot 2>/dev/null}
 function a() {addr2line -e Disk/boot/PowerNex.krl $1}
