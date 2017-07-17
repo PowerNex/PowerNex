7a34974ef1974a6b00683b13e9babc53de5da5ba
Dan Printzell <xwildn00bx@gmail.com>
2017-07-17 22:53:19 +0200

    env.sh: Run PowerNex with 2 cores
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- env.sh
+++ env.sh
@@ -6,6 +6,6 @@ function initPowerNex() {
 }
 function c() { rm -rf objs powernex.iso && echo "Clean successful" || echo "Clean failed"; }
 function v() { initPowerNex && ninja; }
-function b() { v && qemu-system-x86_64 -cdrom powernex.iso -m 256 -monitor stdio -serial file:COM1.log -no-reboot 2>/dev/null; }
+function b() { v && qemu-system-x86_64 -cdrom powernex.iso -m 256 -monitor stdio -smp cpus=1,cores=2,threads=1,maxcpus=2 -serial file:COM1.log 2>/dev/null; }
 function a() { addr2line -e objs/powernex.iso.objs/disk/boot/powernex.krl $1; }
 function al() { addr2line -e objs/powernex.iso.objs/disk/boot/powerd.ldr $1; }
